d34f05a72c172a19fb2e3e9e3962d9f6afd0dba5
Winn Y Cui <pyrinox@airbears2-10-142-66-78.airbears2.1918.berkeley.edu>
2017-09-29 11:51:00 -0700

    updated url in notifier.rb

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ApplicationMailer
 
   def notify(teacher)
     @teacher = teacher
-    @url = 'http://cpits.herokuapp.com/users/sign_in'
+    @url = 'http://capits.herokuapp.com/users/sign_in'
     # change from tina@cpits.org to cpits@mailinator.com to avoid spamming tina
     mail(to: 'cpits@mailinator.com',
          subject: 'A New Poem Has Been Submitted')
